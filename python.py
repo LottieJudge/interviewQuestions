e65e22f8a823384018c39448f880cc56cc9b72de
cactusballs <lottielucyjudge@gmail.com>
2025-03-19 19:44:08 +0000

    creating first iteration

---
+++ python.py
@@ -0,0 +1,21 @@
+import random
+
+StarQuestions = ["Tell me about your proudest professional achievement?", "Describe a situation where you overcame an obstacle?", "Describe a time when you had to work closely with different people?", "Describe a time when your team or company was going through a change and how you dealt with that?", "Tell me about a time you had to work closely with someone whos personality was different from yours?", "Tell me about a time you failed? How did you deal with the situation?", "tell me about a time your responsibilites became overwhelming and what did you do?","what role have you played in team situations?", "Describe a time when you achieved a goal you orginally thought out of reach?"]
+
+otherQuestions = ["What motivates you?", "Why do you want to get into Tech?", "What is your passion outside of tech?", "Would you say your values align with that of the company?"]
+
+def randomQ(arr1, arr2):
+  questionStar = random.choice(arr1)
+  questionNormal = random.choice(arr2)
+
+  questionOne = input(f"{questionStar}")
+  file = open("starQuestionAnswers", "w")
+  file.write(questionOne)
+  file.close()
+
+  questionTwo = input(f"Thanks so much, and {questionNormal}")
+  file = open("otherQuestionsAnswers", "w")
+  file.write(questionTwo)
+  file.close()
+
+randomQ(StarQuestions, otherQuestions)
