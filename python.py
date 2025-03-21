86ca6b8c915c0d9d111fbcfab4253bf36fbb6177
cactusballs <lottielucyjudge@gmail.com>
2025-03-21 06:39:21 +0000

    Updatiing names for clean code

--- python.py
+++ python.py
@@ -2,21 +2,21 @@ import random
 
 StarQuestions = ["Tell me about your proudest professional achievement?", "Describe a situation where you overcame an obstacle?", "Describe a time when you had to work closely with different people?", "Describe a time when your team or company was going through a change and how you dealt with that?", "Tell me about a time you had to work closely with someone whos personality was different from yours?", "Tell me about a time you failed? How did you deal with the situation?", "tell me about a time your responsibilites became overwhelming and what did you do?","what role have you played in team situations?", "Describe a time when you achieved a goal you orginally thought out of reach?"]
 
-otherQuestions = ["What motivates you?", "Why do you want to get into Tech?", "What is your passion outside of tech?", "Would you say your values align with that of the company?", "how do you deal with difficult tasks?", "Who's your hero?", "Whats your favourite saying?", "Why our company?", "Where do you see yourself in Ten Years?", "What is your approach to problem solving?", "What is your biggest weakness?"]
+generalQuestions = ["What motivates you?", "Why do you want to get into Tech?", "What is your passion outside of tech?", "Would you say your values align with that of the company?", "how do you deal with difficult tasks?", "Who's your hero?", "Whats your favourite saying?", "Why our company?", "Where do you see yourself in Ten Years?", "What is your approach to problem solving?", "What is your biggest weakness?"]
 
 def randomQ(arr1, arr2):
   questionStar = random.choice(arr1)
-  questionNormal = random.choice(arr2)
+  questionGeneral = random.choice(arr2)
 
   questionOne = input(f"{questionStar}")
-  file = open("starQuestionAnswers", "a")
+  file = open("starQuestionAnswers.txt", "a")
   file.write(questionOne)
   file.close()
 
-  questionTwo = input(f"Thanks so much, and {questionNormal}")
-  file = open("otherQuestionsAnswers", "a")
+  questionTwo = input(f"Thanks so much, and {questionGeneral}")
+  file = open("otherQuestionsAnswers.txt", "a")
   file.write(questionTwo)
   file.close()
 
-randomQ(StarQuestions, otherQuestions)
+randomQ(StarQuestions, generalQuestions)
 
